9d361d3fc6184e8bdbabf6d12903eb28ce2a3690
kostuk42 <kostuk42@i.ua>
2021-07-16 11:33:26 +0300

    Updates

---
+++ static/js/main.32043de5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{2:function(t,o,s){"use strict";s.r(o);s(0),s(4)}},[[2,1,2]]]);
+//# sourceMappingURL=main.32043de5.chunk.js.map
